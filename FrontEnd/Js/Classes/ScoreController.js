3947c800703eebbbf6378c04af3b9b95c251bf45
Dadadam <bogdanov.andrey.code@gmail.com>
2022-12-25 19:58:10 +0300

    Start  ajax work

--- FrontEnd/Js/Classes/ScoreController.js
+++ FrontEnd/Js/Classes/ScoreController.js
@@ -2,16 +2,31 @@ class ScoreController
 {
     constructor() // конутруктор класса это стандартная функция которая выполняется когда создается класс через new
     {
-      this.counter = 0;
+        this.counter = 0;
     }
 
     increment()
     {
-      this.counter++;
+        this.counter++;
     }
 
     render()
     {
-      return "Счет: " + this.counter;
+        return "Счет: " + this.counter;
+    }
+
+    sendScore()
+    {
+      let request = await fetch(
+          '/BackEnd/Api/Score/Set/', //сюда путь до принимающего php скрипта
+          {
+              method: 'POST',
+              credentials: 'include',
+              body: data // в data хранятся передаваемые данные например json
+          }
+      );
+
+      if (request.ok)
+        // запрос прошёл.
     }
 }
